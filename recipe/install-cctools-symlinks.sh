7febfc4b270436b812f42481dabb83658f692240
scott hain <shain@anaconda.com>
2025-03-28 15:31:14 -0700

    Fix versioning, update patches, some other fixes
    Signed-off-by: scott hain <shain@anaconda.com>

--- recipe/install-cctools-symlinks.sh
+++ recipe/install-cctools-symlinks.sh
@@ -2,6 +2,6 @@ prefix="${macos_machine}-"
 
 pushd $PREFIX/bin
   for tool in $(ls ${prefix}*); do
-    ln -s $PREFIX/bin/$tool $PREFIX/bin/${tool:${#prefix}} || true
+    ln -s $PREFIX/bin/$tool $PREFIX/bin/${tool:${#prefix}}
   done
 popd
